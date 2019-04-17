c7741f5db26c95375e36d9ea52310754dd931834
FreeLeft <47333425+freeleft@users.noreply.github.com>
2019-04-17 07:40:36 -0300

    Change porta

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var app = require('./config/custom-express')();
 
-app.listen(8081, function(){
-  console.log('Servidor rodando na porta 8081.');
+app.listen(5000, function(){
+  console.log('Servidor rodando na porta 5000.');
 });
